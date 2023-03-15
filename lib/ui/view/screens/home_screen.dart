1598d166e29c2d486ecf28e4a7fd3282d8d745a3
esatozkan <esatozkn@gmail.com>
2023-03-15 14:23:31 +0300

    added bg

---
+++ lib/ui/view/screens/home_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(),
+    );
+  }
+}
