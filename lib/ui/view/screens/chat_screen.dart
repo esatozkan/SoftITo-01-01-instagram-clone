fd4f4d1eef28db7dd0c12a20a376dde6a03d5a22
Samet Yılmaz <sametylmz621@gmail.com>
2023-03-17 02:00:29 +0300

    .

--- lib/ui/view/screens/chat_screen.dart
+++ lib/ui/view/screens/chat_screen.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
 
 class ChatScreen extends StatelessWidget {
   const ChatScreen({super.key});
@@ -8,6 +6,7 @@ class ChatScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(),
       body: Column(),
     );
   }
