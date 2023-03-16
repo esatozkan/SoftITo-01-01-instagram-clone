2f94b4d106996ea24f9a51b904a4bee74dc843d9
Samet Yılmaz <sametylmz621@gmail.com>
2023-03-17 01:38:25 +0300

    added reels screen

---
+++ lib/ui/view/screens/reels_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class ReelsScreen extends StatelessWidget {
+  const ReelsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(),
+    );
+  }
+}
