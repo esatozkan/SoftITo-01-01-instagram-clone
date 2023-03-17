d24bfd789e46df5e16ee383c6011ce71dba37305
Samet Yılmaz <sametylmz621@gmail.com>
2023-03-17 16:03:48 +0300

    .

--- lib/ui/view/widgets/reels_post.dart
+++ lib/ui/view/widgets/reels_post.dart
@@ -1,9 +1,34 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:video_player/video_player.dart';
 
-class ReelsPost extends StatelessWidget {
+class ReelsPost extends StatefulWidget {
   const ReelsPost({super.key});
+
+  @override
+  State<ReelsPost> createState() => _ReelsPostState();
+}
+
+class _ReelsPostState extends State<ReelsPost> {
+  late VideoPlayerController _videoPlayerController;
+  @override
+  void initState() {
+    _videoPlayerController = VideoPlayerController.network(
+        'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4');
+
+    _videoPlayerController.addListener(() {
+      setState(() {});
+    });
+    _videoPlayerController.setLooping(true);
+    _videoPlayerController.initialize().then((_) => setState(() {}));
+    _videoPlayerController.play();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _videoPlayerController.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,7 +36,11 @@ class ReelsPost extends StatelessWidget {
       itemCount: 20,
       shrinkWrap: true,
       physics: const NeverScrollableScrollPhysics(),
-      itemBuilder: (context, index) {},
+      itemBuilder: (context, index) {
+        return AspectRatio(
+          aspectRatio: _videoPlayerController.value.aspectRatio,
+        );
+      },
     );
   }
 }
